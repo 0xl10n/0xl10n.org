491a8fca3b0110834d3589f733f8345ee39bd83d
Justin Lau <justin@tclau.com>
2024-04-26 02:51:38 +0900

    refactor: vite -> craco

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,37 +3,41 @@ import themer from "@tailus/themer";
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: ["./*.html", "./*.js"],
-    darkMode: "false",
-    safelist: ["isToggled"],
-    theme: {
-      fontFamily: {
-        sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
-        mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
-      },
-        keyframes: {
-            loop: {
-                to: {
-                    "offset-distance": "100%",
-                },
-            },
-        },
-
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "../common/src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "false",
+  safelist: ["isToggled"],
+  theme: {
+    fontFamily: {
+      sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
+      mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
     },
-    plugins: [
-        themer({
-            palette: {
-                extend : "nature"
-            },
-            radius: "smoothest",
-            background: "light",
-            border: "light",
-            padding:"large",
-            components: {
-                button: {
-                    rounded : "2xl"
-                }
-            }
-        })
-    ],
-};
+    keyframes: {
+      loop: {
+        to: {
+          "offset-distance": "100%",
+        },
+      },
+    },
+
+  },
+  plugins: [
+    themer({
+      palette: {
+        extend: "nature",
+      },
+      radius: "smoothest",
+      background: "light",
+      border: "light",
+      padding: "large",
+      components: {
+        button: {
+          rounded: "2xl",
+        },
+      },
+    }),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ["light"],
+  },
+}
