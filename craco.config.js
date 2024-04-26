10759959e6f1d69d09505f092e9f38e6a8893bd1
debuggingfuture <lauchunyin@debuggingfuture.com>
2024-04-26 15:58:33 +0800

    fix: tests setup

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,31 @@
-module.exports = {};
+const esmModules = [
+    "@rainbow-me",
+    "@spruceid",
+    "wagmi",
+    "@wagmi",
+    "github\\.com\\+gitcoinco\\+allo\\-indexer\\-client",
+];
+
+module.exports = {
+    jest: {
+        configure: () => ({
+            preset: "ts-jest/presets/js-with-ts",
+            testEnvironment: "jsdom",
+            clearMocks: true,
+            resetMocks: true,
+            coverageProvider: "v8",
+            verbose: true,
+            // @rainbow-me/rainbowkit is already an ESM module and
+            // it trips Jest when it tries to transform it, this ignores it
+            transformIgnorePatterns: [
+                `/node_modules/.pnpm/(?!(${esmModules.join("|")}))`,
+            ],
+            moduleNameMapper: {
+                "\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$":
+                    require.resolve("jest-transform-stub"),
+            },
+            setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+            testPathIgnorePatterns: ["/e2e/"],
+        }),
+    },
+};
