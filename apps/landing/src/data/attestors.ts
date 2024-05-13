c4514dce319a593c365386df19fd14affa91087e
debuggingfuture <lauchunyin@debuggingfuture.com>
2024-05-14 06:18:21 +0800

    fix: fixtures

--- apps/landing/src/data/attestors.ts
+++ apps/landing/src/data/attestors.ts
@@ -20,7 +20,6 @@ export const ATTESTORS_DATA = [
     description:
       "Mastering 7 languages, she has experience working in the field of translation for 5 years. Currently picking up Thai in Bangkok",
     score: 64.5,
-    
     wordCount: 7430,
     locales: [Locale.EN, Locale.ZH_TW, Locale.JA],
   },
@@ -30,7 +29,7 @@ export const ATTESTORS_DATA = [
     profileUrl: "https://pbs.twimg.com/profile_images/1163460536840753154/pSwBx1q7_400x400.jpg",
     description:
       "Mastering 7 languages, she has experience working in the field of translation for 5 years. Currently improving his Japanese",
-    score: 24.5,
+    score: 28.5,
     wordCount: 5430,
     locales: [Locale.EN, Locale.ES_ES],
   },
@@ -40,7 +39,7 @@ export const ATTESTORS_DATA = [
     profileUrl: "https://pbs.twimg.com/profile_images/1786244534088949760/DxPdShTw_400x400.jpg",
     description:
       "Experience working in Vietnam",
-    score: 34.5,
+    score: 31.5,
     wordCount: 710,
     locales: [Locale.EN, Locale.VI],
   },
@@ -51,7 +50,6 @@ export const ATTESTORS_DATA = [
     description:
       "Experienced Tech consultant who is currently residing in Kyoto, Japan.",
     score: 54.1,
-    
     wordCount: 2430,
     locales: [Locale.EN, Locale.JA],
   },
